4a408ff10a3035a65a7229d32f138feab1a5617f
moreira <moreira@zaia.com>
2024-04-11 20:17:18 -0300

    feat

--- system/wm/hyprland.nix
+++ system/wm/hyprland.nix
@@ -5,9 +5,51 @@
   programs.hyprland = {
     # Install the packages from nixpkgs
     enable = true;
-    # Whether to enable XWayland
+    nvidiaPatches = true;
     xwayland.enable = true;
   };
+
+
+  # environment.sessionVariables = {
+  #   #If your cursor becomes invisible
+  #   WLR_NO_HARDWARE_CURSORS = "1";
+  #   Hint electron apps to use wayland
+  #   NIXOS_OZONE_WL = "1";
+  # };
+
+  environment.systemPackages = [
+    pkgs.waybar
+    pkgs.eww
+    pkgs.dunst
+    libnotify
+    hyprpaper
+    swaybg
+    mpvpaper
+    swww
+    kitty
+    wezterm # testar esse 
+    rofi-wayland
+    wofi
+    bemenu
+    fuzzel
+    tofi
+
+
+    (pkgs.waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+      })
+    )
+
+  ];
+
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+
+
+  # rofi keybind
+  bind = $mainMod, S, exec, rofi -show drun -show-icons
 
 
     
