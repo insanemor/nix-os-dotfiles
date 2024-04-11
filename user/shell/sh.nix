8f27370af920e6db3fc75658996833a90379ce63
moreira@zaia.com.br <moreira@zaia.com.br>
2024-04-11 17:19:06 -0300

    test

--- user/shell/sh.nix
+++ user/shell/sh.nix
@@ -36,12 +36,9 @@ in
         "jsontools"
       ];
     };
-    promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
     initExtra = ''
-    PROMPT=" ◉ %U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
-     %F{green}→%f "
-    RPROMPT="%F{red}▂%f%F{yellow}▄%f%F{green}▆%f%F{cyan}█%f%F{blue}▆%f%F{magenta}▄%f%F{white}▂%f"
-    [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
+    source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+
     '';
   };
 
