924673b95647dfdc44853945ff8261639a46570d
moreira@zaia.com.br <moreira@zaia.com.br>
2024-04-11 17:59:12 -0300

    fix

--- user/app/vscode/vscode.nix
+++ user/app/vscode/vscode.nix
@@ -2,12 +2,22 @@
 
 {
 
-  environment.systemPackages = with pkgs; [ vscode ];
-
-
-  programs.vscode = {
-    enable = true;
-    package = pkgs.vscode.fhs;
-  };
+  environment.systemPackages = with pkgs; [
+  (vscode-with-extensions.override {
+    vscodeExtensions = with vscode-extensions; [
+      bbenoist.nix
+      ms-python.python
+      ms-azuretools.vscode-docker
+      ms-vscode-remote.remote-ssh
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "remote-ssh-edit";
+        publisher = "ms-vscode-remote";
+        version = "0.47.2";
+        sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+      }
+    ];
+  })
+];
 
 }
