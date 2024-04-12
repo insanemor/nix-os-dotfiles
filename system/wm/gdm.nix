ec02af8f199fa51b29cc5ff3352c6331355572a7
moreira@zaia.com.br <moreira@zaia.com.br>
2024-04-12 11:35:32 -0300

    fix

--- system/wm/gdm.nix
+++ system/wm/gdm.nix
@@ -2,15 +2,11 @@
 
 {
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
+    enable = true;
+    desktopManager.gnome.enable = true;
     xkb = {
       layout = "br";
       variant = "";
