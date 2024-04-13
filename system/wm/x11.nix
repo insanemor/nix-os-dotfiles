e2975d8d35b5a28120c6312c890064640edd4b5f
moreira <moreira@zaia.com>
2024-04-13 12:49:29 -0300

    fix

--- system/wm/x11.nix
+++ system/wm/x11.nix
@@ -16,17 +16,33 @@
     # ./sway/sway.nix
   ];
 
-  services.xserver = {
-    enable = true;
-    desktopManager.wallpaper.mode = "scale";
-    # scaling of fonts and graphical elements on the screen
-    dpi = 98;
-    libinput.enable =
-      true; # Enable touchpad support (enabled default in most desktopManager).
+  services = {
+    xserver = {
+      enable = true;
+      desktopManager.wallpaper.mode = "scale";
+      # scaling of fonts and graphical elements on the screen
+      dpi = 98;
+      libinput.enable =
+        true; # Enable touchpad support (enabled default in most desktopManager).
 
-    desktopManager = {
-      plasma5.enable = true;
+      desktopManager = {
+        plasma5.enable = true;
+      };
+     
+
+      # ---------------------------------------------------------------------
+      # Vidoe settings that go hand-in-hand with opengl
+      # ---------------------------------------------------------------------
+      videoDrivers = [
+        "fbdev" # The fbdev (Framebuffer Device) driver is a generic framebuffer driver that provides access to the frame buffer of the display hardware.
+        # "modesetting"     # The modesetting driver is a generic driver for modern video hardware that relies on kernel modesetting (KMS) to set the display modes and manage resolution and refresh rate.
+        # "amdgpu"          # This is the open-source kernel driver for AMD graphics cards. It's part of the AMDGPU driver stack and provides support for newer AMD GPUs.
+        # "nouveau"         # Nouveau is an open-source driver for NVIDIA graphics cards. It aims to provide support for NVIDIA GPUs and is an alternative to the proprietary NVIDIA driver
+        # "radeon"          # The Radeon driver is an open-source driver for AMD Radeon graphics cards. It provides support for older AMD GPUs.
+        "nvidia"
+      ];
     };
+
     displayManager = {
       sddm = {
         enable = true;
@@ -34,17 +50,6 @@
       };
     };
 
-    # ---------------------------------------------------------------------
-    # Vidoe settings that go hand-in-hand with opengl
-    # ---------------------------------------------------------------------
-    videoDrivers = [
-      "fbdev" # The fbdev (Framebuffer Device) driver is a generic framebuffer driver that provides access to the frame buffer of the display hardware.
-      # "modesetting"     # The modesetting driver is a generic driver for modern video hardware that relies on kernel modesetting (KMS) to set the display modes and manage resolution and refresh rate.
-      # "amdgpu"          # This is the open-source kernel driver for AMD graphics cards. It's part of the AMDGPU driver stack and provides support for newer AMD GPUs.
-      # "nouveau"         # Nouveau is an open-source driver for NVIDIA graphics cards. It aims to provide support for NVIDIA GPUs and is an alternative to the proprietary NVIDIA driver
-      # "radeon"          # The Radeon driver is an open-source driver for AMD Radeon graphics cards. It provides support for older AMD GPUs.
-      "nvidia"
-    ];
   };
 
   # Old notes to self
