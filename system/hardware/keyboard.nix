6539f2428b94e060c3ef453ad33753f3158528ca
moreira <moreira@zaia.com>
2024-04-14 12:50:18 -0300

    test keyboard

--- system/hardware/keyboard.nix
+++ system/hardware/keyboard.nix
@@ -1,8 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-  
+  services.xserver = {
+    autoRepeatDelay = 600;
+    autoRepeatInterval = 25;
+    layout = "br";
+    xkb = {
+        Variant = "thinkpad";
+        xkbModel = "thinkpadz60";
+    };
+  };
 
-
-  
+  console.useXkbConfig = true;
 }
