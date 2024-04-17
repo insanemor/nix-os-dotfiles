7457b27055b1bad259ebb6ecaadab110f9d50884
moreira@zaia.com.br <moreira@zaia.com.br>
2024-04-16 23:42:51 -0300

    fix

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
       userSettings = rec {
         username = "ins";                                             # username
         name = "moreira";                                             # name/identifier
-        email = "";                                # email (used for certain configurations)
+        email = "moreira@zaia.com.br";                                # email (used for certain configurations)
         dotfilesDir = "~/.dotfiles";                                  # absolute path of the local repo
         theme = "uwunicorn-yt";                                       # selcted theme from my themes directory (./themes/)
         wm = "hyprland";                                               # Selected window manager or desktop environment; must select one in both ./user/wm/ and ./system/wm/
