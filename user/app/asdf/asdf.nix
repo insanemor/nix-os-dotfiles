fb54142eaeb646044f9e1d66d13f0870f27b87cf
moreira@zaia.com.br <moreira@zaia.com.br>
2024-04-24 18:11:19 -0300

    fix

--- user/app/asdf/asdf.nix
+++ user/app/asdf/asdf.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, userSettings, systemSettings, ... }:
 let
  myAliases = {
-    nasdf = "/.dotfiles/user/app/asdf/nasdf.sh";
+    nasdf = "~/.dotfiles/user/app/asdf/nasdf.sh";
   };
 in
 {
