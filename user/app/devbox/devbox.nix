2916094935aee4f8f2335e37b83301fae62601a7
moreira@zaia.com.br <moreira@zaia.com.br>
2024-04-25 18:28:15 -0300

    test devconbtainer

--- user/app/devbox/devbox.nix
+++ user/app/devbox/devbox.nix
@@ -5,17 +5,52 @@
 
   #xdg.configFile."~/dconf/.keep".source = builtins.toFile "keep" ""; 
 
-  home.file."/dconf/.keep".text = ''
-      text
+  home.file."/dconf/default/devbox.json".text = ''
+    {
+      "packages": [],
+      "shell": {
+        "init_hook": [
+          "echo 'Welcome to devbox!' > /dev/null"
+        ],
+      }
+
+    }
   
   '';
 
 
-  # home.file.".tool-versions".text = ''
-  #     helm 3.14.3
-  #     terraform 1.0.11
-  #     nodejs 20.5.1
+  home.file."/dconf/default/.devcontainer/devcontainer.json".text = ''
+    {
+      "name": "Ferramentas de Desenvolvimento",
+      
+      "image": "insanemor/tools:3.6",
+      
+      "service": "dev_container",
+      
+      "workspaceFolder": "/work",
+      
+      "settings": { 
+        "terminal.integrated.shell.linux": "/bin/zsh"
+      },
+      
+      "extensions": [
+        "ms-vscode-remote.remote-containers"
+      ],
+      
+      "remoteUser": "ins",
+
+      "runArgs": [
+        "--net=host",
+        "--memory=1024mb",
+        "--userns=keep-id",
+        "-u", "${localEnv:UID}:${localEnv:UID}",
+        "-v", "/home/ins/GIT:/work"
+      ]
+
+      
+    }
   
-  # '';
+  '';
+
 
 }
