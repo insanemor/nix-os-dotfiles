e57bdc111c5309b950ba7a1929100d218d2898f9
moreira@zaia.com.br <moreira@zaia.com.br>
2024-04-26 11:37:00 -0300

    test devbox

--- user/app/devbox/devbox.nix
+++ user/app/devbox/devbox.nix
@@ -7,12 +7,26 @@
 
   home.file."/dconf/default/devbox.json".text = ''
     {
-      "packages": [],
+      "packages": [
+        "awscli2@2.15.18",
+        "kubernetes-helm@3.14.4",
+        "k9s@0.32.4",
+        "kubectl@1.29.4",
+        "kubectx@0.9.5",
+        "terraform@1.8.1",
+        "terraformer@0.8.24",
+        "terragrunt@0.56.5",
+        "argocd@2.10.7",
+        "sops@3.8.1",
+      ],
       "shell": {
         "init_hook": [
-          "echo 'Welcome to devbox!' > /dev/null"
+          "echo 'Welcome to devbox!'"
         ],
-      }
+      },
+      "scripts": {
+
+      },
 
     }
   
