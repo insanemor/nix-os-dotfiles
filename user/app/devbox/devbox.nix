22f648d449a20cc44f2a38e347b31ff8f3911b03
moreira@zaia.com.br <moreira@zaia.com.br>
2024-04-26 11:17:05 -0300

    feat: separa o devbox do devcontainer

--- user/app/devbox/devbox.nix
+++ user/app/devbox/devbox.nix
@@ -19,38 +19,6 @@
   '';
 
 
-  home.file."/dconf/default/.devcontainer/devcontainer.json".text = ''
-    {
-      "name": "Ferramentas de Desenvolvimento",
-      
-      "image": "docker.io/insanemor/tools:3.6",
-      
-      "service": "dev_container",
-      
-      "workspaceFolder": "/work",
-      
-      "settings": { 
-        "terminal.integrated.shell.linux": "/bin/zsh"
-      },
-      
-      "extensions": [
-        "ms-vscode-remote.remote-containers"
-      ],
-      
-      "remoteUser": "ins",
-
-      "runArgs": [
-        "--net=host",
-        "--memory=1024mb",
-        "--userns=keep-id",
-        "-u", "1000:1000",
-        "-v", "/home/ins/GIT:/work"
-      ]
-
-      
-    }
-  
-  '';
 
 
 }
