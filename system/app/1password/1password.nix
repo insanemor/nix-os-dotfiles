cc1df79c09ab67b751e1c135b31c1c885a3e4baa
moreira <moreira@zaia.com.br>
2024-04-27 19:23:23 -0300

    add plugin shell

--- system/app/1password/1password.nix
+++ system/app/1password/1password.nix
@@ -15,7 +15,10 @@
     enable = true;
   };
 
-  
+    # Enable 1password plugins on interactive shell init
+  programs.zsh.interactiveShellInit = ''
+    source /home/ins/.config/op/plugins.sh
+  '';
   
 
   # Useful otherdevelopment tools
