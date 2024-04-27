b65d65e3a53fb7ba5184485c463a06691b80e2c1
moreira <moreira@zaia.com.br>
2024-04-27 17:52:48 -0300

    password ok

--- system/app/1password/1password.nix
+++ system/app/1password/1password.nix
@@ -7,11 +7,20 @@
 
   programs._1password-gui = {
     enable = true;
+    polkitPolicyOwners = [ "ins" ];
   };
 
 
+  security.polkit = {
+    enable = true;
+  };
+
+  
+  
+
   # Useful otherdevelopment tools
   # environment.systemPackages = with pkgs; [
+    
   # ];
 
 }
