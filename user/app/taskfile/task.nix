e0c7d7867c5f3ae4021ad9af9cf112da0b6a054c
moreira@zaia.com.br <moreira@zaia.com.br>
2024-04-28 07:56:39 -0300

    add taskfile test

--- user/app/taskfile/task.nix
+++ user/app/taskfile/task.nix
@@ -2,5 +2,17 @@
 
 {
 
-  
+  home.file."/dconf/default/taskfile.json".text = ''
+
+    version: '3'
+
+    tasks:
+      hello:
+        silent: true
+        cmds:
+          - echo "Hello from the remote Taskfile!"
+
+
+  '';
+
 }
