5418fcd0a822f6dc632c2c91c403ecc7681e8464
moreira@zaia.com.br <moreira@zaia.com.br>
2024-04-29 15:09:24 -0300

    test modulos in nixos

--- profiles/personal/secrets.nix
+++ profiles/personal/secrets.nix
@@ -1,14 +1,24 @@
 { lib, config, pkgs, userSettings, options, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = userSettings.username;
-  home.homeDirectory = "/home/"+userSettings.username;
+  
+  options = {
+    secrets.enable = 
+      lib.mkEnableOption "Enable secrets";
+  };
 
-  imports = [ 
-    #../clientes/SBIT/nixos/main.nix
-  ];
+  config = lib.mkIf options.secrets.enable {
+
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    home.username = userSettings.username;
+    home.homeDirectory = "/home/"+userSettings.username;
+
+    imports = [ 
+      #../clientes/SBIT/nixos/main.nix
+    ];
+
+  };
 
 
 }
