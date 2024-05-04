fd06650c37cee4d1592cede8264004d81eb25f81
moreira <moreira@zaia.com.br>
2024-05-04 18:25:01 -0300

    add app stow

--- profiles/personal/home.nix
+++ profiles/personal/home.nix
@@ -16,6 +16,7 @@
     ../basic/home.nix # Personal is essentially work system
     ../devops/home.nix
     ../../user/app/libreoffice/libreoffice.nix
+    ../../user/app/stow/atow.nix
   ]; #++ lib.optional (builtins.pathExists ./secrets.nix) ./secrets.nix;
 
   home.stateVersion = "23.11"; # Please read the comment before changing.
